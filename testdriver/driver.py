19439e2780bebb6f4dcd36f69d57d3756e1f02f8
Thiago de Paula Souza <thiagodepaulasouza@gmail.com>
2022-04-19 00:52:14 -0300

    ex1 solução

--- testdriver/driver.py
+++ testdriver/driver.py
@@ -1,3 +1,16 @@
+#cada chave do valor devera ser o valor de atributo
+#cada valor deve ser o texto contido no elemento
 from selenium import webdriver
-driver = webdriver.Chrome(executable_path='D:\\chromeDriver\\chromedriver.exe')
-driver.get('https://www.youtube.com')
+from time import sleep
+navegador = webdriver.Chrome(executable_path='D:\\chromeDriver\\chromedriver.exe')
+url = 'https://curso-python-selenium.netlify.app/exercicio_01.html'
+navegador.get(url)
+sleep(1)
+
+titulo = navegador.find_element_by_tag_name('h1')
+
+print("({" f"'{titulo.text}'" ': ' ' {')
+for i in range(3):
+    p = navegador.find_elements_by_tag_name('p')
+    print(f"    'texto{i+1}'" ': ' f"'{p[i].text}'")
+print('}})')
